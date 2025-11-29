7e53b81c95215e88e89e938f7a551efb06c9ac11
Luiz Enrique <luizenrique80025030@gmail.com>
2025-11-29 15:24:59 -0300

    Corrigindo os ícones de hamburger nas páginas de usuário e adiministrador, travando o scroll quando a aba do perfil está aberta, adicionando transição a troca de estado da aba de perfil e corrigindo os links do footer no desktop.

--- la_belle_bijou/static/js/scripts.js
+++ la_belle_bijou/static/js/scripts.js
@@ -1,3 +1,4 @@
+// Modificiar o estado do header mobile conforme cliques
 document.addEventListener('DOMContentLoaded', function() {
     const navbarToggler = document.querySelector('.custom-toggler');
     const navbarCollapse = document.getElementById('navbarMobile');
@@ -11,7 +12,7 @@ document.addEventListener('DOMContentLoaded', function() {
             navbarCollapse.classList.add('show');
             overlay.classList.add('show');
             body.classList.add('menu-open');
-            body.style.overflow = 'hidden';
+            body.style.position = 'fixed';
             navbarToggler.setAttribute('aria-expanded', 'true');
             navbarToggler.classList.add('bac')
         } else {
@@ -23,7 +24,7 @@ document.addEventListener('DOMContentLoaded', function() {
         navbarCollapse.classList.remove('show');
         overlay.classList.remove('show');
         body.classList.remove('menu-open');
-        body.style.overflow = '';
+        body.style.position = '';
         navbarToggler.setAttribute('aria-expanded', 'false');
     }
 
@@ -41,23 +42,34 @@ document.addEventListener('DOMContentLoaded', function() {
     });
 });
 
+// Ativar e dastivar o modal do perfil do usuário
 icon_perfil = document.getElementById('icone-perfil')
 menu_variavel = document.getElementById('menu_variavel')
-menu_variavel.style.display = 'none'
 overlay = document.getElementById('overlay')
 
+function impedirScroll() {
+    const largura_scroll_bar = window.innerWidth - document.documentElement.clientWidth;
+    document.documentElement.style.overflow = 'hidden';
+    document.body.style.paddingRight = largura_scroll_bar + 'px';
+}
+
+function liberarScroll() {
+    document.documentElement.style.overflow = '';
+    document.body.style.paddingRight = '';
+}
+
 icon_perfil.addEventListener('click', () => {
-    menu_variavel.classList.toggle('oculto')
-    menu_variavel.style.display = 'flex'
-    overlay.classList.toggle('oculto')
+    menu_variavel.classList.toggle('visivel')
+    overlay.classList.toggle('visivel')
     icon_perfil.classList.toggle('ativo')
     document.body.classList.toggle('no_scroll')
+    impedirScroll()
 })
 
 overlay.addEventListener('click', () => {
-    menu_variavel.classList.toggle('oculto')
-    menu_variavel.style.display = 'none'
-    overlay.classList.toggle('oculto')
+    menu_variavel.classList.toggle('visivel')
+    overlay.classList.toggle('visivel')
     icon_perfil.classList.toggle('ativo')
     document.body.classList.toggle('no_scroll')
+    liberarScroll()
 })
