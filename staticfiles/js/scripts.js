87c520587b98fd5b1cb641d61a6b693c664a0ce7
Lucas-Beni <lucgarcbeni@gmail.com>
2025-12-03 13:31:10 -0300

    adicionadas imagens da loggi e jadlog

--- staticfiles/js/scripts.js
+++ staticfiles/js/scripts.js
@@ -1,3 +1,4 @@
+// Modificiar o estado do header mobile conforme cliques
 document.addEventListener('DOMContentLoaded', function() {
     const navbarToggler = document.querySelector('.custom-toggler');
     const navbarCollapse = document.getElementById('navbarMobile');
@@ -41,23 +42,34 @@ document.addEventListener('DOMContentLoaded', function() {
     });
 });
 
+// Ativar e dastivar o modal do perfil do usuário
 icon_perfil = document.getElementById('icone-perfil')
 menu_variavel = document.getElementById('menu_variavel')
-menu_variavel.style.display = 'none'
 overlay = document.getElementById('overlay')
 
+function impedirScroll() {
+    const largura_scroll_bar = window.innerWidth - document.documentElement.clientWidth;
+    document.documentElement.style.overflow = 'hidden';
+    document.body.style.paddingRight = largura_scroll_bar + 'px';
+}
+
+function liberarScroll() {
+    document.documentElement.style.overflow = '';
+    document.body.style.paddingRight = '';
+}
+
 icon_perfil.addEventListener('click', () => {
-    menu_variavel.classList.toggle('oculto')
-    menu_variavel.style.display = 'flex'
-    overlay.classList.toggle('oculto')
+    menu_variavel.classList.toggle('visivel')
+    overlay.classList.toggle('visivel')
     icon_perfil.classList.toggle('ativo')
     document.body.classList.toggle('no_scroll')
+    impedirScroll()
 })
 
 overlay.addEventListener('click', () => {
-    menu_variavel.classList.toggle('oculto')
-    menu_variavel.style.display = 'none'
-    overlay.classList.toggle('oculto')
+    menu_variavel.classList.toggle('visivel')
+    overlay.classList.toggle('visivel')
     icon_perfil.classList.toggle('ativo')
     document.body.classList.toggle('no_scroll')
+    liberarScroll()
 })
