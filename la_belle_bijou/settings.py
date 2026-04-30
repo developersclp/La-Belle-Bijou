f214e4a8f60ede1986d213193afaaeb344eb74e3
developersclp <developersclp@gmail.com>
2026-04-30 20:34:13 -0300

    Update settings.py

--- la_belle_bijou/settings.py
+++ la_belle_bijou/settings.py
@@ -83,6 +83,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 
     "allauth.account.middleware.AccountMiddleware",
     'accounts.middleware.CompleteProfileMiddleware',
