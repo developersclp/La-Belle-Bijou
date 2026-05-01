d2216a1a020478be1cdf909090c5ff88b38385c1
developersclp <developersclp@gmail.com>
2026-04-30 21:47:21 -0300

    Update settings.py

--- la_belle_bijou/settings.py
+++ la_belle_bijou/settings.py
@@ -41,8 +41,8 @@ STATICFILES_DIRS = [
 ]
 
 # Arquivos de mídia (uploads de imagens)
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+#MEDIA_URL = '/media/'
+#MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
